c1693ae52c97573bf321111a28fa5433e77df00b
zhoujie <zhoujie02@vanke.com>
2017-09-07 14:05:31 +0800

    测试远程提交

--- src/main/java/com/zj/tets/git/App.java
+++ src/main/java/com/zj/tets/git/App.java
@@ -10,7 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         
-
+        System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
     }
 }
