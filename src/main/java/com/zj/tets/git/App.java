884954ae5808030271f22e7597d7aad8c28292bd
zhoujie <zhoujie02@vanke.com>
2017-09-07 13:42:31 +0800

    创建项目

---
+++ src/main/java/com/zj/tets/git/App.java
@@ -0,0 +1,13 @@
+package com.zj.tets.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
