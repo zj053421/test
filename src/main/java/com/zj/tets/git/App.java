24e94728ecc966763e71419f142a51af92f0dd6f
zhoujie02 <zhoujie02@vanke.com>
2019-03-20 15:22:24 +0800

    分支测试项目

--- src/main/java/com/zj/tets/git/App.java
+++ src/main/java/com/zj/tets/git/App.java
@@ -8,11 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        //注释
         System.out.println( "Hello World!" );
-        
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        /////
     }
 }
