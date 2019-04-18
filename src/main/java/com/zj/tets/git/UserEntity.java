0f8f96d1d2703acd561719a8bccbde1bb276e4e4
zhoujie02 <zhoujie02@vanke.com>
2019-04-18 16:16:55 +0800

    增加sout

---
+++ src/main/java/com/zj/tets/git/UserEntity.java
@@ -0,0 +1,21 @@
+package com.zj.tets.git;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+public class UserEntity {
+
+  @Setter
+  @Getter
+  private  String userName;
+
+  @Setter
+  @Getter
+  private  int age;
+
+
+}
+
